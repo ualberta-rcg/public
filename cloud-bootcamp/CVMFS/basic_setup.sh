7e42e1c17f52474cb08967a76b87da90ba56b344
EPei <erming.pei@gmail.com>
2023-05-17 15:10:40 -0600

    Update basic_setup.sh

--- cloud-bootcamp/CVMFS/basic_setup.sh
+++ cloud-bootcamp/CVMFS/basic_setup.sh
@@ -6,7 +6,7 @@ sudo yum -y install cvmfs cvmfs-config-default cvmfs-config-computecanada cvmfs-
 wegt https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest_all.deb
 sudo dpkg -i cvmfs-release-latest_all.deb
 sudo apt-get update
-sudo apt-get install cvmfs
+sudo apt-get -y install cvmfs
 
 # For Fedora
 sudo dnf install https://ecsft.cern.ch/dist/cvmfs/cvmfs-2.10.1/cvmfs-2.10.1-1.fc34.x86_64.rpm https://ecsft.cern.ch/dist/cvmfs/cvmfs-config/cvmfs-config-default-latest.noarch.rpm
