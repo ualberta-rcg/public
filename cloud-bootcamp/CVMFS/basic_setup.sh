e967d1df7b5334136bd720fcc6278de9640a3819
EPei <erming.pei@gmail.com>
2023-05-17 11:14:41 -0600

    Update basic_setup.sh

--- cloud-bootcamp/CVMFS/basic_setup.sh
+++ cloud-bootcamp/CVMFS/basic_setup.sh
@@ -9,4 +9,5 @@ sudo bash -c 'cat > /etc/cvmfs/default.local' << EOF
 CVMFS_REPOSITORIES="cvmfs-config.computecanada.ca,soft.computecanada.ca" 
 CVMFS_STRICT_MOUNT="yes"
 CVMFS_QUOTA_LIMIT=10000 # Adjust this value according to the storage space on the VM
-CVMFS_HTTP_PROXY="http://cvmfs-cache.arbutus.cloud.computecanada.ca:3128;DIRECT"  
+CVMFS_HTTP_PROXY="http://cvmfs-cache.arbutus.cloud.computecanada.ca:3128;DIRECT"
+EOF
